516326e7229de6c69788fae56feab0cf03081ccb
bean980310 <bean980310@hotmail.co.kr>
2024-11-06 09:53:47 +0900

    add api input code

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 from binance.client import Client
 import pandas as pd
+from pathlib import Path
+from getpass import getpass
+
+api_key_file=Path.home()/'.binance'/'api_key.txt'
+api_secret_file=Path.home()/'.binance'/'api_secret.txt'
+
+def get_api_key():
+    try:
+        with open(api_key_file, 'r') as f:
+            return f.read().strip()
+    except Exception as e:
+        input_api_key()
+        
+def set_api_key(api_key):
+    if not api_key_file.parent.exists():
+        api_key_file.parent.mkdir(parents=True)
+    with open(api_key_file, 'w') as f:
+        f.write(api_key)
+
+def input_api_key():
+    api_key = input("Enter your Binance API key: ")
+    set_api_key(api_key)
+    return api_key
+
+def get_api_secret():
+    try:
+        with open(api_secret_file, 'r') as f:
+            return f.read().strip()
+    except Exception as e:
+        input_api_secret()
+
+def set_api_secret(api_secret):
+    if not api_secret_file.parent.exists():
+        api_secret_file.parent.mkdir(parents=True)
+    with open(api_secret_file, 'w') as f:
+        f.write(api_secret)
+
+def input_api_secret():
+    api_secret = getpass("Enter your Binance API secret: ")
+    set_api_secret(api_secret)
+    return api_secret
 
 # API 키 설정 (보안을 위해 환경 변수나 별도 파일에서 불러오는 것을 권장)
-api_key = 'your_api_key'
-api_secret = 'your_api_secret'
+api_key = get_api_key()
+api_secret = get_api_secret()
 client = Client(api_key, api_secret)
 
 # 히스토리컬 캔들 데이터 가져오기
@@ -98,4 +139,4 @@ while True:
     # ...
     # 차트 업데이트 또는 데이터 저장
     # ...
-    time.sleep(60)  # 1분마다 업데이트
+    # time.sleep(60)  # 1분마다 업데이트
